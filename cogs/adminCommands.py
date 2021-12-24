d3ccc64783c4e0aa917b5cd1c597eef32d9439e1
alexx2021 <78677978+alexx2021@users.noreply.github.com>
2021-12-23 16:08:31 -0800

    add try/finally for both commands

--- cogs/adminCommands.py
+++ cogs/adminCommands.py
@@ -18,82 +18,87 @@ class Admin(commands.Cog):
     @has_permissions(administrator=True)
     @commands.command()
     async def accept(self, ctx):
-        row = await self.bot.db.execute_fetchall('SELECT * FROM tickets WHERE channel_id = ?',(ctx.channel.id,))
-        if row:
-            messages = await ctx.channel.history(limit=200).flatten()
 
-            buffer = io.BytesIO()
-            theContent = ''
-            for message in messages:
-                theContent += f'{message.author} ({message.author.id}): {message.content}\n'
-            buffer = io.BytesIO(theContent.encode("utf8"))
-            
-            e = discord.Embed(color=discord.Color.green(), title=f'{ctx.channel.name} Accepted')
-            e.timestamp = discord.utils.utcnow()
-            
-            file = discord.File(fp=buffer, filename=f"{message.guild.id}-{message.channel.id}.txt")
+        try:
+            row = await self.bot.db.execute_fetchall('SELECT * FROM tickets WHERE channel_id = ?',(ctx.channel.id,))
+            if row:
+                messages = await ctx.channel.history(limit=200).flatten()
 
-            applog = self.bot.get_channel(APP_LOG_CHANNEL) 
-            await applog.send(file=file, embed = e)
-            buffer.close()
+                buffer = io.BytesIO()
+                theContent = ''
+                for message in messages:
+                    theContent += f'{message.author} ({message.author.id}): {message.content}\n'
+                buffer = io.BytesIO(theContent.encode("utf8"))
+                
+                e = discord.Embed(color=discord.Color.green(), title=f'{ctx.channel.name} Accepted')
+                e.timestamp = discord.utils.utcnow()
+                
+                file = discord.File(fp=buffer, filename=f"{message.guild.id}-{message.channel.id}.txt")
+
+                applog = self.bot.get_channel(APP_LOG_CHANNEL) 
+                await applog.send(file=file, embed = e)
+                buffer.close()
 
 
-            user_id = int(row[0][0])
-            member = await get_or_fetch_member(self, ctx.guild, user_id)
-            await member.remove_roles(discord.utils.get(ctx.guild.roles, name="Applicant"))
-            await member.add_roles(discord.utils.get(ctx.guild.roles, name="Member"))
-            await ctx.channel.delete()
+                user_id = int(row[0][0])
+                member = await get_or_fetch_member(self, ctx.guild, user_id)
+                await member.remove_roles(discord.utils.get(ctx.guild.roles, name="Applicant"))
+                await member.add_roles(discord.utils.get(ctx.guild.roles, name="Member"))
+                await ctx.channel.delete()
 
-            try:
-                await member.send(f"Your application has been accepted by {ctx.author}! The server IP is `play.sleepless.community`.")
-            except Exception as e:
-                pass
+                generalchat = self.bot.get_channel(GENERAL_CHAT)
+                await generalchat.send(f"Welcome {member.mention}! Make sure to read <#{RULES_CHANNEL}>.")
 
-            generalchat = self.bot.get_channel(GENERAL_CHAT)
-            await generalchat.send(f"Welcome {member.mention}! Make sure to read <#{RULES_CHANNEL}>.")
+                try:
+                    await member.send(f"Your application has been accepted by {ctx.author}! The server IP is `play.sleepless.community`.")
+                except Exception as e:
+                    pass
+
+            else:
+                await ctx.send("This isn't an application channel")
+        finally:
             await self.bot.db.execute('DELETE FROM tickets WHERE channel_id = ?',(ctx.channel.id,))
             await self.bot.db.commit()
-        else:
-            await ctx.send("This isn't an application channel")
 
     @has_permissions(administrator=True)
     @commands.command()
     async def deny(self, ctx, *, reason: str = None):
-        row = await self.bot.db.execute_fetchall('SELECT * FROM tickets WHERE channel_id = ?',(ctx.channel.id,))
-        if row:
-            messages = await ctx.channel.history(limit=200).flatten()
+        try:
+            row = await self.bot.db.execute_fetchall('SELECT * FROM tickets WHERE channel_id = ?',(ctx.channel.id,))
+            if row:
+                messages = await ctx.channel.history(limit=200).flatten()
 
-            buffer = io.BytesIO()
-            theContent = ''
-            for message in messages:
-                theContent += f'{message.author} ({message.author.id}): {message.content}\n'
-            buffer = io.BytesIO(theContent.encode("utf8"))
-            
-            e = discord.Embed(color=discord.Color.red(), title=f'{ctx.channel.name} Denied')
-            e.timestamp = discord.utils.utcnow()
-            
-            file = discord.File(fp=buffer, filename=f"{message.guild.id}-{message.channel.id}.txt")
+                buffer = io.BytesIO()
+                theContent = ''
+                for message in messages:
+                    theContent += f'{message.author} ({message.author.id}): {message.content}\n'
+                buffer = io.BytesIO(theContent.encode("utf8"))
+                
+                e = discord.Embed(color=discord.Color.red(), title=f'{ctx.channel.name} Denied')
+                e.timestamp = discord.utils.utcnow()
+                
+                file = discord.File(fp=buffer, filename=f"{message.guild.id}-{message.channel.id}.txt")
 
-            applog = self.bot.get_channel(APP_LOG_CHANNEL) 
-            await applog.send(file=file, embed = e)
-            buffer.close()
+                applog = self.bot.get_channel(APP_LOG_CHANNEL) 
+                await applog.send(file=file, embed = e)
+                buffer.close()
 
-            await ctx.channel.delete()
+                await ctx.channel.delete()
 
-            user_id = int(row[0][0])
-            member = await get_or_fetch_member(self, ctx.guild, user_id)
+                user_id = int(row[0][0])
+                member = await get_or_fetch_member(self, ctx.guild, user_id)
 
 
-            try:
-                await member.send(f"Your application has been denied by {ctx.author}. Reason: `{reason}`")
-            except Exception as e:
-                pass
+                try:
+                    await member.send(f"Your application has been denied by {ctx.author}. Reason: `{reason}`")
+                except Exception as e:
+                    pass
 
+            else:
+                await ctx.send("This isn't an application channel")
+        finally:
             await self.bot.db.execute('DELETE FROM tickets WHERE channel_id = ?',(ctx.channel.id,))
             await self.bot.db.commit()
-
-        else:
-            await ctx.send("This isn't an application channel")
 
 
 
